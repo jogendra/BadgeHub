43eaeee709b9cfbb15640eeb7a7dcbdfec70d652
Ben Osheroff <ben@gimbo.net>
2020-08-22 12:06:37 -0700

    add custom source path

--- Package.swift
+++ Package.swift
@@ -41,7 +41,9 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "BadgeHub",
-            dependencies: [])
+            dependencies: [],
+            sources: ["BadgeHub"]
+        )
     ]
 )
 
